332e4775bc23b5f819db57a4c80b6cd4adf135aa
ValeCentena <qf8p2jnmjc@privaterelay.appleid.com>
2026-06-12 20:45:40 -0300

    Agregar BrowserRouter en main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 const rootElement = document.getElementById('root');
@@ -9,7 +10,9 @@ if (!rootElement) {
 } else {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
   );
 }
