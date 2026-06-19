49a409c80f6cf33a60cb8a105b5f88782e059701
ValeCentena <qf8p2jnmjc@privaterelay.appleid.com>
2026-06-19 20:40:49 -0300

    Convertir panel profesional en PWA instalable

---
+++ frontend/public/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "tuagendaya-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/profesional/login",
+  "/admin/login",
+  "/manifest.webmanifest",
+  "/tuagendaya-logo.png",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).catch(() => {
+        return Promise.resolve();
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
