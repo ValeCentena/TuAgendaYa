5c54116413914f97027cf13764f638a96fb0cee0
ValeCentena <qf8p2jnmjc@privaterelay.appleid.com>
2026-07-12 19:23:24 -0300

    Remove upper WhatsApp button from client card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3273,7 +3273,6 @@ function ReservationsSection() {
                       </div>
 
                       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, minmax(0, 1fr))', gap: 8 }}>
-
                         <button
                           type="button"
                           onClick={copyClientPhone}
